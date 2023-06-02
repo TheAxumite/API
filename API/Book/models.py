3b28ca3847a73caf529a8a8fbc3874e659654720
Leul Dessalegn <100012855+TheAxumite@users.noreply.github.com>
2023-06-02 01:34:43 +0000

    Update committed

--- API/Book/models.py
+++ API/Book/models.py
@@ -4,6 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
-
+    
     def __str__(self):
         return self.title
+    
--- API/Book/urls.py
+++ API/Book/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from Book.views import BookList
+from Book.consumer import SocketConsumer
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    re_path(r'ws/socket/$', SocketConsumer.as_asgi()),
 ]
