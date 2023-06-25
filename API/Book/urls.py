540d2220eefadb8c0a998844136b67d7890e237d
Leul Dessalegn <100012855+TheAxumite@users.noreply.github.com>
2023-06-25 22:20:21 +0000

    Update

--- API/Book/urls.py
+++ API/Book/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from Book.views import BookList
+
 from Book.consumer import SocketConsumer
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    
     path('ws/socket/', SocketConsumer.as_asgi()),
 ]
